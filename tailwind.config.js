9c6c29755504fbd43199b0061548a8f521c72182
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-06-15 21:31:12 -0300

    [Deploy] previous commit fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,tsx,jsx}"],
   darkMode: false, // or 'media' or 'class'
+  mode: "jit",
   theme: {
     keyframes: {
       linearProgress: {
